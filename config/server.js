170dea02675d0a5802a779fbde7353cbadce2054
Silas Silva <silas.silva@rockt.com.br>
2019-07-26 13:29:06 -0300

    - Adicionando helmet e cors

--- config/server.js
+++ config/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 const multipart = require('connect-multiparty')
+const helmet = require('helmet')
+const cors = require('cors')
 
 const businessRoutes = require('../src/routes/business')
 
@@ -10,6 +12,8 @@ app.use(bodyParser.json({ limit: '250mb' }))
 app.use(bodyParser.urlencoded({ limit: '250mb', extended: true }))
 app.use(expressValidator())
 app.use(multipart())
+app.use(helmet())
+app.use(cors())
 
 businessRoutes(app)
 
