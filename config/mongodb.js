f77779d74e31226fd1d554a44fe48713e514c33d
Luiz Duarte <luiz.asus@gmail.com>
2019-09-18 12:03:32 -0300

    Alteração configuração do mongodb, add um if para verificar se existe a variavel de ambiente user e pass para que rode local

--- config/mongodb.js
+++ config/mongodb.js
@@ -6,7 +6,9 @@ async function connect () {
   if (connection) return null
 
   const result = await new Promise((resolve, reject) => {
-    MongoClient.connect(`mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`, (err, conn) => {
+    let connectionMongo = ''
+    process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD ? connectionMongo = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}` : connectionMongo = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+    MongoClient.connect(connectionMongo, (err, conn) => {
       if (err) {
         resolve({ err, db: null })
       } else {
