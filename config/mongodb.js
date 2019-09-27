a49210abe8808d33a514e48ae97ff2e3f2dfeb47
Silas Silva <silas.silva@rockt.com.br>
2019-09-26 22:06:31 -0300

    Correçao na string de conexao

--- config/mongodb.js
+++ config/mongodb.js
@@ -7,7 +7,7 @@ async function connect () {
 
   const result = await new Promise((resolve, reject) => {
     let connectionMongo = ''
-    process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD ? connectionMongo = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}` : connectionMongo = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
+    process.env.MONGO_USERNAME && process.env.MONGO_PASSWORD ? connectionMongo = `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}` : connectionMongo = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`
     MongoClient.connect(connectionMongo, (err, conn) => {
       if (err) {
         resolve({ err, db: null })
