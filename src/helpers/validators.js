c87c20a5f48e7eb0573525683cb7292bd552d1b1
Silas Silva <silas.silva@rockt.com.br>
2020-01-14 12:26:27 -0300

    Correção na validação de e-mail

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -5,8 +5,8 @@ function mongoIdIsValid (id = null) {
 }
 
 function validateEmail (email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase())
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(String(email).toLowerCase())) return true
+  return false
 }
 
 function isArrayObject (array) {
