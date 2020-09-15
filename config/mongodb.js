1976c12b1aad277e559717776fbbfb54a028ad2a
Silas silva <silas.silva@digitalk.com.br>
2020-09-15 13:48:19 -0300

    Adicionando parametro adicional no .env para ser incluido na string de conexão do mongodb

--- config/mongodb.js
+++ config/mongodb.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient
 
 async function connect (app, callback) {
   let connectionMongo = ''
-  const config = { username: '', password: '', host: '', port: '', database: '' }
+  const config = { username: '', password: '', host: '', port: '', database: '', additionalParams: '' }
 
   if (process.env.NODE_ENV === 'test') {
     config.username = process.env.MONGO_USERNAME_TEST
@@ -10,19 +10,23 @@ async function connect (app, callback) {
     config.host = process.env.MONGO_HOST_TEST
     config.port = process.env.MONGO_PORT_TEST
     config.database = process.env.MONGO_DATABASE_TEST
+    config.additionalParams = process.env.MONGO_TEST_STRINGCONNECTION_PARAMS || ''
   } else {
     config.username = process.env.MONGO_USERNAME
     config.password = process.env.MONGO_PASSWORD
     config.host = process.env.MONGO_HOST
     config.port = process.env.MONGO_PORT
     config.database = process.env.MONGO_DATABASE
+    config.additionalParams = process.env.MONGO_STRINGCONNECTION_PARAMS || ''
   }
 
   if (config.username && config.password) {
     connectionMongo = `mongodb://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`
   } else {
-    connectionMongo = `mongodb://${config.host}:${config.port}`
+    connectionMongo = `mongodb://${config.host}:${config.port}/`
   }
+  if (config.additionalParams) connectionMongo = `${connectionMongo}?${config.additionalParams}`
+  console.log('connection_string', connectionMongo)
 
   MongoClient.connect(connectionMongo, { promiseLibrary: Promise }, (err, conn) => {
     if (err) console.error(`#00000 - Falha ao conectar ao banco de dados. ${err.stack}`)
