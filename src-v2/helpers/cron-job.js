d8d1c59f8653b90fe0f01d04e6b2153b9382ed48
Silas Silva <silas.silva@digitalk.com.br>
2021-09-10 16:49:52 -0300

    fix: Adicionando variavel de ambiente para habilitar/desabilitar rotina que desativa mailings vencidos

--- src-v2/helpers/cron-job.js
+++ src-v2/helpers/cron-job.js
@@ -3,4 +3,8 @@ const BusinessController = require('../controllers/business-controller')
 
 const businessController = new BusinessController(null)
 
-const deactivateExpiredBusiness = new Cron('50 23 * * *', async () => await businessController.deactivateExpiredBusiness(), null, true)
+const enableAutoDisableExpired = (process.env.ENABLE_AUTO_DISABLE_MAILING_EXPIRED) ? process.env.ENABLE_AUTO_DISABLE_MAILING_EXPIRED : false
+if (String(enableAutoDisableExpired) === 'true') {
+    const deactivateExpiredBusiness = new Cron('50 23 * * *', async () => await businessController.deactivateExpiredBusiness(), null, true)
+}
+
