c7691e835d3350664518359fd4d1935f778314cc
Silas <silas.silva@digitalk.com.br>
2022-03-09 18:42:38 -0300

    fix: Removendo variavel desnecessaria

--- src-v2/helpers/cron-job.js
+++ src-v2/helpers/cron-job.js
@@ -6,6 +6,6 @@ export function runCronJob() {
 
   const enableAutoDisableExpired = process.env.ENABLE_AUTO_DISABLE_MAILING_EXPIRED ? process.env.ENABLE_AUTO_DISABLE_MAILING_EXPIRED : false
   if (String(enableAutoDisableExpired) === 'true') {
-    const deactivateExpiredBusiness = new Cron('50 23 * * *', async () => await businessController.deactivateExpiredBusiness(), null, true)
+    new Cron('50 23 * * *', async () => await businessController.deactivateExpiredBusiness(), null, true)
   }
 }
