9812008d515dfb76ee67633a2d5f4f1bea3c7f22
Silas S <silas.silva@digitalk.com.br>
2023-07-31 16:09:00 -0300

    fix: correção para o apm funcionar apenas em ambiente de produção

--- config/elastic-apm.js
+++ config/elastic-apm.js
@@ -6,14 +6,17 @@ const projectName = process.env.PROJECT_NAME
 const apmServerUrl = process.env.APM_SERVER_URL
 const environment = process.env.APP_ENVIRONMENT
 
-let tracing = apm.start({
-    serviceName:  projectName,
-    serverUrl:  apmServerUrl,
+let tracing
+if (process.env.STATE_ENV === 'production') {
+  tracing = apm.start({
+    serviceName: projectName,
+    serverUrl: apmServerUrl,
     environment: environment,
     pageLoadTraceId: '${transaction.traceId}',
     pageLoadSpanId: '${transaction.ensureParentId()}',
-    pageLoadSampled: '${transaction.sampled}',
+    pageLoadSampled: '${transaction.sampled}'
     //logLevel: 'debug',
-})
+  })
+}
 
 export default tracing
